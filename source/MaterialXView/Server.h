9d11b7ae4dc5cd63cc3c897b8862ca9684157b4b
Ilia Bozhinov <ammen99@gmail.com>
2024-08-15 14:43:49 +0200

    migrate to using separate controller class for drogon

--- source/MaterialXView/Server.h
+++ source/MaterialXView/Server.h
@@ -70,6 +70,189 @@ inline Json::Value variableToJson(std::string name, float min, float max)
     return v;
 }
 
+class ServerController : public drogon::HttpController<ServerController>
+{
+  public:
+    METHOD_LIST_BEGIN
+    METHOD_ADD(ServerController::reset, "/reset");
+    METHOD_ADD(ServerController::getshader, "/getshader");
+    METHOD_ADD(ServerController::getuniforms, "/getuniforms");
+    METHOD_ADD(ServerController::metrics, "/metrics");
+    METHOD_ADD(ServerController::setshader, "/setshader");
+    METHOD_ADD(ServerController::screenshot, "/screenshot");
+    METHOD_LIST_END
+
+    void reset(const drogon::HttpRequestPtr& req,
+        std::function<void (const drogon::HttpResponsePtr &)> &&callback)
+    {
+        ng::async([=] () mutable {
+            std::cout << "Reset shader!" << std::endl;
+            viewer->getSelectedMaterial()->generateShader(viewer->getGenContext());
+            callback(drogon::HttpResponse::newHttpResponse());
+        });
+    }
+
+    void getshader(const drogon::HttpRequestPtr& req,
+        std::function<void (const drogon::HttpResponsePtr &)> &&callback)
+    {
+        ng::async([=] () mutable {
+            Json::Value r;
+            if (auto material = viewer->getSelectedMaterial())
+            {
+                r["vertex"] = material->getShader()->getSourceCode(mx::Stage::VERTEX);
+                r["fragment"] = material->getShader()->getSourceCode(mx::Stage::PIXEL);
+            }
+
+            callback(drogon::HttpResponse::newHttpJsonResponse(r));
+        });
+    }
+
+    void getuniforms(const drogon::HttpRequestPtr& req,
+        std::function<void (const drogon::HttpResponsePtr &)> &&callback)
+    {
+        ng::async([=] () mutable {
+            Json::Value r = Json::arrayValue;
+            r.append(variableToJson("cameraX", -3, 3));
+            r.append(variableToJson("cameraY", -3, 3));
+            r.append(variableToJson("cameraZ", -3, 3));
+
+            if (auto material = viewer->getSelectedMaterial())
+            {
+                if (material->getPublicUniforms())
+                {
+                    auto& uniforms = *material->getPublicUniforms();
+                    for (size_t i = 0; i < uniforms.size(); i++) {
+                        std::cout << debug(i) << std::endl;
+                        std::cout << debug(uniforms[i]->getName()) << std::endl;
+                        std::cout << debug(uniforms[i]->getType().getName()) << std::endl;
+                        r.append(variableToJson(uniforms[i]->getName(), 0, 1));
+                    }
+                }
+            }
+
+            callback(drogon::HttpResponse::newHttpJsonResponse(r));
+        });
+    }
+
+    void setshader(const drogon::HttpRequestPtr& _req,
+        std::function<void (const drogon::HttpResponsePtr &)> &&callback)
+    {
+
+        auto req = _req->getJsonObject();
+        if (!req) {
+            std::cout << "Invalid request: /setshader" << std::endl;
+            callback(drogon::HttpResponse::newHttpResponse(drogon::k400BadRequest,
+                    drogon::ContentType::CT_TEXT_HTML));
+            return;
+        }
+        std::cout << "Got request for set shader" << std::endl;
+
+        ng::async([=] () mutable
+        {
+            std::cout << "Dispatching request for set shader" << std::endl;
+
+            if (auto material = viewer->getSelectedMaterial())
+            {
+                auto old_vertex = material->getShader()->getSourceCode(mx::Stage::VERTEX);
+                auto old_fragment = material->getShader()->getSourceCode(mx::Stage::PIXEL);
+
+                auto vertex = req->get("vertex", old_vertex).asString();
+                auto fragment = req->get("fragment", old_fragment).asString();
+
+                auto error = set_shader_from_source(viewer, vertex, fragment);
+                if (error == "")
+                {
+                    std::cout << "Successfully set shader!" << std::endl;
+                    callback(drogon::HttpResponse::newHttpResponse());
+                } else
+                {
+                    set_shader_from_source(viewer, old_vertex, old_fragment);
+                    auto resp = drogon::HttpResponse::newHttpResponse(drogon::k418ImATeapot,
+                        drogon::ContentType::CT_TEXT_PLAIN);
+                    resp->setBody(error);
+                    callback(resp);
+                }
+            } else {
+                std::cout << "Missing selected material ?!?" << std::endl;
+                auto resp = drogon::HttpResponse::newHttpResponse(drogon::k418ImATeapot,
+                    drogon::ContentType::CT_TEXT_PLAIN);
+                resp->setBody("unknown error");
+                callback(resp);
+            }
+        });
+    }
+
+    void screenshot(const drogon::HttpRequestPtr& _req,
+        std::function<void (const drogon::HttpResponsePtr &)> &&callback)
+    {
+        auto req = _req->getJsonObject();
+        if (!req) {
+            std::cout << "Invalid request: /screenshot" << std::endl;
+            callback(drogon::HttpResponse::newHttpResponse(drogon::k400BadRequest,
+                    drogon::ContentType::CT_TEXT_HTML));
+            return;
+        }
+
+        double x = req->get("x", viewer->getCameraPosition()[0]).asDouble();
+        double y = req->get("y", viewer->getCameraPosition()[1]).asDouble();
+        double z = req->get("z", viewer->getCameraPosition()[2]).asDouble();
+        std::cout << "Pending screenshot: " << " x: " << x << " y: " << y << " z: " << z << " " << glfwGetTime() << std::endl;
+
+        ng::async([=] () mutable {
+            viewer->setCameraPosition(mx::Vector3(x, y, z));
+            auto img = viewer->getNextRender();
+
+            auto resp = drogon::HttpResponse::newHttpResponse();
+            resp->setContentTypeCode(drogon::CT_CUSTOM);
+
+            int width = img->getWidth();
+            int height = img->getHeight();
+            resp->addCookie("width", std::to_string(width));
+            resp->addCookie("height", std::to_string(height));
+            unsigned char* data = (unsigned char*)img->getResourceBuffer();
+            resp->setBody(std::string(data, data + width * height * 3));
+            callback(resp);
+            std::cout << "Screenshot taken: " << glfwGetTime() << std::endl;
+        });
+    }
+
+    void metrics(const drogon::HttpRequestPtr& _req,
+            std::function<void (const drogon::HttpResponsePtr &)> &&callback) {
+
+        auto req = _req->getJsonObject();
+        if (!req) {
+            std::cout << "Invalid request: /metrics" << std::endl;
+            callback(drogon::HttpResponse::newHttpResponse(drogon::k400BadRequest,
+                    drogon::ContentType::CT_TEXT_HTML));
+            return;
+        }
+
+        uint width = req->get("width", 1024).asUInt();
+        uint height = req->get("height", 1024).asUInt();
+        uint nr_frames = req->get("frames", 100).asInt();
+        uint warmup = req->get("warmup-frames", 100).asInt();
+
+        if (width == 0 || height == 0 || width > 8192 || height > 8192) {
+            std::cout << "Malformed request: " << width << " " << height << std::endl;
+            callback(drogon::HttpResponse::newHttpResponse(drogon::k400BadRequest, drogon::ContentType::CT_TEXT_PLAIN));
+            return;
+        }
+
+        ng::async([=] () mutable {
+            std::cout << "Running benchmark " << width << " " << height << " " << nr_frames << std::endl;
+            GLuint speed = viewer->runBenchmark(warmup, nr_frames, width, height);
+            std::cout << "Results: " << speed << std::endl;
+            Json::Value r;
+            r["speed"] = speed;
+            callback(drogon::HttpResponse::newHttpJsonResponse(r));
+        });
+    }
+
+    ng::ref<Viewer> viewer;
+};
+
+
+
 /**
  * A simple class which encapsulates the state used for setting the shader and current material remotely.
  */
@@ -108,176 +291,7 @@ class Server {
             .setLogLevel(trantor::Logger::kWarn)
             .addListener("0.0.0.0", port)
             .setThreadNum(1)
-
-            .registerHandler("/reset", [=] (
-                    const drogon::HttpRequestPtr& req,
-                    std::function<void (const drogon::HttpResponsePtr &)> &&callback) mutable {
-                ng::async([=] () mutable {
-                    std::cout << "Reset shader!" << std::endl;
-                    viewer->getSelectedMaterial()->generateShader(viewer->getGenContext());
-                    callback(drogon::HttpResponse::newHttpResponse());
-                });
-            })
-
-            .registerHandler("/getshader", [=] (
-                    const drogon::HttpRequestPtr& req,
-                    std::function<void (const drogon::HttpResponsePtr &)> &&callback){
-
-                ng::async([=] () mutable {
-                    Json::Value r;
-                    if (auto material = viewer->getSelectedMaterial())
-                    {
-                        r["vertex"] = material->getShader()->getSourceCode(mx::Stage::VERTEX);
-                        r["fragment"] = material->getShader()->getSourceCode(mx::Stage::PIXEL);
-                    }
-
-                    callback(drogon::HttpResponse::newHttpJsonResponse(r));
-                });
-            })
-
-            .registerHandler("/getuniforms", [=] (
-                    const drogon::HttpRequestPtr& req,
-                    std::function<void (const drogon::HttpResponsePtr &)> &&callback){
-
-                ng::async([=] () mutable {
-                    Json::Value r = Json::arrayValue;
-                    r.append(variableToJson("cameraX", -3, 3));
-                    r.append(variableToJson("cameraY", -3, 3));
-                    r.append(variableToJson("cameraZ", -3, 3));
-
-                    if (auto material = viewer->getSelectedMaterial())
-                    {
-                        if (material->getPublicUniforms())
-                        {
-                            auto& uniforms = *material->getPublicUniforms();
-                            for (size_t i = 0; i < uniforms.size(); i++) {
-                                std::cout << debug(i) << std::endl;
-                                std::cout << debug(uniforms[i]->getName()) << std::endl;
-                                std::cout << debug(uniforms[i]->getType().getName()) << std::endl;
-                                r.append(variableToJson(uniforms[i]->getName(), 0, 1));
-                            }
-                        }
-                    }
-
-                    callback(drogon::HttpResponse::newHttpJsonResponse(r));
-                });
-            })
-
-            .registerHandler("/setshader", [=] (
-                    const drogon::HttpRequestPtr& _req,
-                    std::function<void (const drogon::HttpResponsePtr &)> &&callback){
-
-                auto req = _req->getJsonObject();
-                if (!req) {
-                    std::cout << "Invalid request: /setshader" << std::endl;
-                    callback(drogon::HttpResponse::newHttpResponse(drogon::k400BadRequest,
-                            drogon::ContentType::CT_TEXT_HTML));
-                    return;
-                }
-                std::cout << "Got request for set shader" << std::endl;
-
-                ng::async([=] () mutable
-                {
-                    std::cout << "Dispatching request for set shader" << std::endl;
-
-                    if (auto material = viewer->getSelectedMaterial())
-                    {
-                        auto old_vertex = material->getShader()->getSourceCode(mx::Stage::VERTEX);
-                        auto old_fragment = material->getShader()->getSourceCode(mx::Stage::PIXEL);
-
-                        auto vertex = req->get("vertex", old_vertex).asString();
-                        auto fragment = req->get("fragment", old_fragment).asString();
-
-                        auto error = set_shader_from_source(viewer, vertex, fragment);
-                        if (error == "")
-                        {
-                            std::cout << "Successfully set shader!" << std::endl;
-                            callback(drogon::HttpResponse::newHttpResponse());
-                        } else
-                        {
-                            set_shader_from_source(viewer, old_vertex, old_fragment);
-                            auto resp = drogon::HttpResponse::newHttpResponse(drogon::k418ImATeapot,
-                                drogon::ContentType::CT_TEXT_PLAIN);
-                            resp->setBody(error);
-                            callback(resp);
-                        }
-                    } else {
-                        std::cout << "Missing selected material ?!?" << std::endl;
-                        auto resp = drogon::HttpResponse::newHttpResponse(drogon::k418ImATeapot,
-                            drogon::ContentType::CT_TEXT_PLAIN);
-                        resp->setBody("unknown error");
-                        callback(resp);
-                    }
-                });
-            })
-
-            .registerHandler("/screenshot", [viewer] (
-                    const drogon::HttpRequestPtr& _req,
-                    std::function<void (const drogon::HttpResponsePtr &)> &&callback) mutable {
-
-                auto req = _req->getJsonObject();
-                if (!req) {
-                    std::cout << "Invalid request: /screenshot" << std::endl;
-                    callback(drogon::HttpResponse::newHttpResponse(drogon::k400BadRequest,
-                            drogon::ContentType::CT_TEXT_HTML));
-                    return;
-                }
-
-                double x = req->get("x", viewer->getCameraPosition()[0]).asDouble();
-                double y = req->get("y", viewer->getCameraPosition()[1]).asDouble();
-                double z = req->get("z", viewer->getCameraPosition()[2]).asDouble();
-                std::cout << "Pending screenshot: " << " x: " << x << " y: " << y << " z: " << z << " " << glfwGetTime() << std::endl;
-
-                ng::async([=] () mutable {
-                    viewer->setCameraPosition(mx::Vector3(x, y, z));
-                    auto img = viewer->getNextRender();
-
-                    auto resp = drogon::HttpResponse::newHttpResponse();
-                    resp->setContentTypeCode(drogon::CT_CUSTOM);
-
-                    int width = img->getWidth();
-                    int height = img->getHeight();
-                    resp->addCookie("width", std::to_string(width));
-                    resp->addCookie("height", std::to_string(height));
-                    unsigned char* data = (unsigned char*)img->getResourceBuffer();
-                    resp->setBody(std::string(data, data + width * height * 3));
-                    callback(resp);
-                    std::cout << "Screenshot taken: " << glfwGetTime() << std::endl;
-                });
-            })
-
-            .registerHandler("/metrics", [=] (
-                    const drogon::HttpRequestPtr& _req,
-                    std::function<void (const drogon::HttpResponsePtr &)> &&callback) mutable {
-
-                auto req = _req->getJsonObject();
-                if (!req) {
-                    std::cout << "Invalid request: /metrics" << std::endl;
-                    callback(drogon::HttpResponse::newHttpResponse(drogon::k400BadRequest,
-                            drogon::ContentType::CT_TEXT_HTML));
-                    return;
-                }
-
-                uint width = req->get("width", 1024).asUInt();
-                uint height = req->get("height", 1024).asUInt();
-                uint nr_frames = req->get("frames", 100).asInt();
-                uint warmup = req->get("warmup-frames", 100).asInt();
-
-                if (width == 0 || height == 0 || width > 8192 || height > 8192) {
-                    std::cout << "Malformed request: " << width << " " << height << std::endl;
-                    callback(drogon::HttpResponse::newHttpResponse(drogon::k400BadRequest, drogon::ContentType::CT_TEXT_PLAIN));
-                    return;
-                }
-
-                ng::async([=] () mutable {
-                    std::cout << "Running benchmark " << width << " " << height << " " << nr_frames << std::endl;
-                    GLuint speed = viewer->runBenchmark(warmup, nr_frames, width, height);
-                    std::cout << "Results: " << speed << std::endl;
-                    Json::Value r;
-                    r["speed"] = speed;
-                    callback(drogon::HttpResponse::newHttpJsonResponse(r));
-                });
-            })
+            .registerController(std::make_shared<ServerController>(viewer))
             .run();
     }
 
